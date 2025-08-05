d226d75c6b4a22720c32ee9a763f45a2a94c1cd7
Kzapka <victorottofgr@gmail.com>
2025-08-05 09:42:22 -0300

    Vue: TypeScript com Composition API

--- Vue/vue-project-components-basics/src/App.vue
+++ Vue/vue-project-components-basics/src/App.vue
@@ -1,36 +1,42 @@
 <script setup>
-import { ref } from 'vue';
 
-//data
-const nome = ref('Victor');
-const link = ref('https://www.scrimba.com');
-const disabled = ref(true);
-const attributes = {
-    id: 'big_emoji',
-    class: 'waving',
-};
-const coding_years = ref(3);
-const getFormattedDate = (date) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString('jp-JP', options);
-};
-const username = ref('rachelktyjohnson');
-const event = ref('dblclick');
-const blockLeave = () => {
-    alert("You can't leave!");
-};
+import Counter from '@/components/Counter.vue'
+import CounterSummary from '@/components/CounterSummary.vue'
+import { ref } from 'vue'
+
+//Data
+const dog_breeds = ref([
+    { label: 'Dalmatian', count: 0 },
+    { label: 'Pug', count: 0 },
+    { label: 'Beagle', count: 0, disabled: true },
+    { label: 'Poodle', count: 0 },
+    { label: 'Bulldog', count: 0, disabled: true },
+])
+
+//HANDLERS
+//Increase count for breed
+const handleIncrement = (breed, amount) => {
+    breed.count += amount
+}
+
+//Decrease count for breed
+const handleDecrement = (breed, amount) => {
+    if (breed.count - amount >= 0) breed.count -= amount
+}
+
+//Reset all counts
+const resetAllCounts = () => {
+    dog_breeds.value.forEach(breed => {
+        breed.count = 0
+    })
+}
+
 </script>
 
 <template>
-    <span v-bind="attributes">👋🏻</span>
-    <h1>Olá, meu nome é
-        <a @click.prevent="blockLeave()" :href="`${link}/@${username}`" target="_blank">{{ nome }}</a>
-    </h1>
-    <a :href="link" target="_blank">
-        <button :disabled>Clique para visitar meu site favorito! Se puder...</button>
-    </a>
-    <h2 @[event]="coding_years++">
-        {{ coding_years > 0 ? `Estou codando há ${coding_years} anos` : "Sou iniciante" }}
-    </h2>
-    <h3>Hoje é {{ getFormattedDate(new Date()) }}</h3>
+    <h1>Dog Park Counter</h1>
+    <Counter v-for="breed in dog_breeds" :label="breed.label" :count="breed.count" :disabled="breed.disabled"
+        @increment="amount => handleIncrement(breed, amount)" @decrement="amount => handleDecrement(breed, amount)"
+        @reset="breed.count = 0" />
+    <CounterSummary counterLabel="breeds" itemLabel="dogs" :data="dog_breeds" @resetAll="resetAllCounts" />
 </template>
